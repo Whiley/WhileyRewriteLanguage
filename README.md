b2911bf6eb3f7a5ac4768d15f731f24d318e82e7
DavePearce <david.pearce@ecs.vuw.ac.nz>
2015-06-17 10:01:25 +1200

    Updates examples to compile and run.
    The examples have now been tested to compile and run, and their README
    files updated.  At this time, the quantifiers example needs to be
    updated.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # WhileyRewriteLanguage
 WyRL is a domain specific rewrite language and code generator which has been custom developed for use within the Whiley Compiler.  Specifically, WyRL is used to generate the Automated Theorem Prover used within Whiley.
+
+# Building
+To build WyRL, you can run ant from the command-line:
+
+```
+> ant
+
+Buildfile: WhileyRewriteLanguage/build.xml
+
+compile-wyrl:
+    [javac] Compiling 1 source file
+     [wyrl] Compiling 0 wyrl file(s)
+    [javac] Compiling 5 source files
+
+build:
+    [mkdir] Created dir: WhileyRewriteLanguage/tmp
+      [jar] Building jar: WhileyRewriteLanguage/lib/wyrl-v0.3.34.jar
+   [delete] Deleting directory WhileyRewriteLanguage/tmp
+     [echo] =============================================
+     [echo] BUILT: lib/wyrl-v0.3.34.jar
+     [echo] =============================================
+
+BUILD SUCCESSFUL
+Total time: 3 seconds
+```
+
+# Examples
+There are several examples provided in the <code>examples/</code> directory:
+<ul>
+
+<li><p><b>Boolean Logic Simplifier.</b>  This simplifies formulae written in propositional logic.  For example, it would simplify <code>X||(Y||!Y)</code> to <code>X</code>.  For more, see <a href="examples/logic/README">here</a>.</p></li>
+
+<li><p><b>Arithmetic Simplifier.</b>  This simplifies simple arithmetic expressions.  For example, it would simplify <code>y-y+(2*x)+(3*x)</code> to <code>5*x</code>.  For more, see <a href="examples/arithmetic/README">here</a>.</p></li>
+
+<li><p><b>Recursive Types Simplifier.</b>  This simplifies types involving unions, intersections and negations and which may also be recursive.  For example, it would simplify <code>(int|!int)&int</code> to <code>int</code>.  For more, see <a href="examples/types/README">here</a>.</p></li>
+
+<li><p><b>Transitive Closure.</b>  This applies transitive closure to strict inequalities.  For example, it would rewrite <code>x < y,y < z</code> to <code>x < y, y < z, x < z</code>.  For more, see <a href="examples/closure/README">here</a>.</p></li>
+
+<li><p><b>Quantifier Instantiater.</b>  This instantiates and simplifies boolean expressions involving quantifiers.  For example, it would simplify <code>fn(x,y) && forall X.!fn(X,X)</code> to <code>False</code>.  For more, see <a href="examples/quantifiers/README">here</a>.</p></li>
+
+</ul>
--- examples/quantifiers/Main.java
+++ examples/quantifiers/Main.java
@@ -7,8 +7,6 @@ import wyautl.io.PrettyAutomataWriter;
 import wyautl.rw.*;
 
 public final class Main {
-    public enum RewriteMode { SIMPLE, STATIC_DISPATCH, GLOBAL_DISPATCH };
-    
     private Main() {} // avoid instantiation of this class
 
     public static void main(String[] args) {
@@ -16,7 +14,6 @@ public final class Main {
 	    new BufferedReader(new InputStreamReader(System.in));
 
 	try {
-	    RewriteMode rwMode = RewriteMode.GLOBAL_DISPATCH;
 	    System.out.println("Welcome!\n");			
 	    while(true) {				
 		System.out.print("> ");
@@ -27,14 +24,8 @@ public final class Main {
 		    printHelp();
 		} else if(text.equals("exit")) {
 		    System.exit(0);
-		} else if(text.equals("rw-simple")) {
-		    rwMode = RewriteMode.SIMPLE;
-		    System.out.println("Rewrite mode: simple");
-		} else if(text.equals("rw-static-dispatch")) {
-		    System.out.println("Rewrite mode: static-dispatch");
-		    rwMode = RewriteMode.STATIC_DISPATCH;
 		} else if(!text.equals("")) {
-		    reduce(text,rwMode);
+		    reduce(text);
 		}
 	    }
 	} catch(IOException e) {
@@ -42,7 +33,7 @@ public final class Main {
 	}
     }
 
-    private static void reduce(String text, RewriteMode rwMode) {
+    private static void reduce(String text) {
 	try {				
 	    Parser parser = new Parser(text);
 	    Automaton automaton = new Automaton();
@@ -55,35 +46,13 @@ public final class Main {
 	    writer.write(automaton);
 	    writer.flush();
 			
-	    StrategyRewriter.Strategy<InferenceRule> inferenceStrategy;
-		StrategyRewriter.Strategy<ReductionRule> reductionStrategy;
-
-		switch (rwMode) {
-		case SIMPLE:
-			inferenceStrategy = new SimpleRewriteStrategy<InferenceRule>(
+	    IterativeRewriter.Strategy<InferenceRule> inferenceStrategy = new SimpleRewriteStrategy<InferenceRule>(
 					automaton, Quantifiers.inferences);
-			reductionStrategy = new SimpleRewriteStrategy<ReductionRule>(
+	    IterativeRewriter.Strategy<ReductionRule> reductionStrategy = new SimpleRewriteStrategy<ReductionRule>(
 					automaton, Quantifiers.reductions);
-			break;
-		case STATIC_DISPATCH:
-			inferenceStrategy = new UnfairStateRuleRewriteStrategy<InferenceRule>(
-					automaton, Quantifiers.inferences,Quantifiers.SCHEMA);
-			reductionStrategy = new UnfairStateRuleRewriteStrategy<ReductionRule>(
-					automaton, Quantifiers.reductions,Quantifiers.SCHEMA);
-		case GLOBAL_DISPATCH:
-			inferenceStrategy = new UnfairRuleStateRewriteStrategy<InferenceRule>(
-					automaton, Quantifiers.inferences);
-			reductionStrategy = new UnfairRuleStateRewriteStrategy<ReductionRule>(
-					automaton, Quantifiers.reductions);	
-			break;
-		default:
-			// DEAD-CODE
-			inferenceStrategy = null;
-			reductionStrategy = null;
-		}
-		StrategyRewriter rw = new StrategyRewriter(automaton,
-				inferenceStrategy, reductionStrategy, Quantifiers.SCHEMA);
-		rw.apply(50,100000);
+	    IterativeRewriter rw = new IterativeRewriter(automaton,
+					 inferenceStrategy, reductionStrategy, Quantifiers.SCHEMA);
+	    rw.apply();
 	    System.out.println("\n\n=> (" + rw.getStats() + ")\n");
 	    writer.write(automaton);
 	    writer.flush();
